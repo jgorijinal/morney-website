c5c771023a1123639eab02a291fb8be0903afd22
jgoriginal <932274204@qq.com>
2021-11-27 21:08:32 +0800

    deploy

---
+++ precache-manifest.f45e61cd61de90ee2c2311c172b230bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3deb3a3ac1aa65d758eb",
+    "url": "/morney-website/css/app.fec5a966.css"
+  },
+  {
+    "revision": "d6633240b9c1fde0e1092bd282ea5976",
+    "url": "/morney-website/img/qrcode.png"
+  },
+  {
+    "revision": "b5af274e772a9ad20f8eb957d1b6c413",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "3deb3a3ac1aa65d758eb",
+    "url": "/morney-website/js/app.18a30e3e.js"
+  },
+  {
+    "revision": "eafefdf4af0f53da0bab",
+    "url": "/morney-website/js/chunk-vendors.cf8e7b1b.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website/robots.txt"
+  }
+]);
